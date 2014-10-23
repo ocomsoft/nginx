44ec33ce087100a99a03d1d7b9432982936ee615
scott warren <scottwarren@ocom.com.au>
2014-10-24 10:43:32 +1100

    Add in Python script to auto generate reverse proxy config

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 sudo docker build -t nginx .
 
-sudo docker run -d -it --name nginx-www -p 80:80 -v /home/ocom/apps/nginx/sites-available:/etc/nginx/sites-enabled -v  /home/ocom/apps/nginx/logs:/var/log/nginx nginx
+mkdir  /home/ocom/docker/nginx/sites-available
+mkdir  /home/ocom/docker/nginx/logs
+
+sudo docker run -d -it --name nginx-www -p 80:80 -v /home/ocom/docker/nginx/sites-available:/etc/nginx/sites-enabled -v  /home/ocom/docker/nginx/logs:/var/log/nginx nginx
 
---
+++ reconfigNginx.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+import docker
+import jinja2
+
+c = docker.Client(base_url='unix://var/run/docker.sock',  version='1.9', timeout=10)
+containers = c.containers()
+
+mappings = {}
+
+for cont in containers:
+#	print cont['Id']
+	
+	container = c.inspect_container(cont['Id'])
+	
+	allEnv = {}
+	
+	for env in container["Config"]["Env"] :
+		e = env.split("=")
+		if len(e) > 1 :
+			allEnv[e[0]] = e[1];
+		
+	if 'vhost' in allEnv :
+		hostMapping = {}
+		
+		if allEnv["vhost"] in mappings :
+			hostMapping = mappings[allEnv["vhost"]]
+		else :
+			hostMapping = []
+			mappings[allEnv["vhost"]] = hostMapping
+			
+		ipAddress = container["NetworkSettings"]["IPAddress"];
+		
+		if "80/tcp" in container["Config"]["ExposedPorts"] :
+			hostMapping.append({'ipaddress': ipAddress, 'internalPort':"80", id:id});
+			
+		if "8080/tcp" in container["Config"]["ExposedPorts"] :
+			hostMapping.append({'ipaddress': ipAddress, 'internalPort':"8080", id:id});
+			
+
+# print mappings
+templateLoader = jinja2.FileSystemLoader( searchpath="/" )
+templateEnv = jinja2.Environment( loader=templateLoader )
+TEMPLATE_FILE = "/home/ocom/docker/reverseProxy.conf.tmpl"
+template = templateEnv.get_template( TEMPLATE_FILE )
+
+#template = Template ("""HostName: {{hostname}} for {% for map in maps %} {{ map.ipaddress }}:{{ map.internalPort }}, {% endfor %}""");
+
+for hostName in mappings :
+	shortHostLabel = hostName.split(".")[0]
+	nginxConf = template.render (hostname=hostName, maps= mappings[hostName], shortHostName=shortHostLabel)
+	filename = "/home/ocom/docker/nginx/sites-available/" + shortHostLabel
+	f = open (filename, 'w')
+	f.write (nginxConf);
+	f.close();
+	print "File for " + shortHostLabel + " generated!"
+
+print "Done!"
